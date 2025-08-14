dc2141e90628cd9455b84b3737998b02d1fc570e
William Marlette <williammarlette3@gmail.com>
2025-08-14 19:03:52 -0400

    update 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,21 @@
 import "./NotFound.css";
+import { AlertTriangle } from "lucide-react";
 
 function NotFound() {
-  return <div className="not-found">NotFound</div>;
+  return (
+    <div className="not-found">
+      <div className="not-found__icon">
+        <AlertTriangle size={48} />
+      </div>
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__message">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <a className="not-found__link" href="/">
+        Go Home
+      </a>
+    </div>
+  );
 }
 
 export default NotFound;
